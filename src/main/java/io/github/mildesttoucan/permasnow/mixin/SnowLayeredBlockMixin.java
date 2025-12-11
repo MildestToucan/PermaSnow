5f432a0da0ab95ecde13613feb1b3cb50501794b
GauntRecluse <introvertpale@gmail.com>
2025-12-11 23:00:19 +0100

    Improve handler names

--- src/main/java/io/github/mildesttoucan/permasnow/mixin/SnowLayeredBlockMixin.java
+++ src/main/java/io/github/mildesttoucan/permasnow/mixin/SnowLayeredBlockMixin.java
@@ -5,9 +5,11 @@ import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.mildesttoucan.permasnow.PermaSnow;
 import net.minecraft.world.level.block.SnowLayerBlock;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+@Debug()
 @Mixin(SnowLayerBlock.class)
 @SuppressWarnings("unused") // Class shows up as unused when abstract due to MCDev bug.
 abstract class SnowLayeredBlockMixin {
@@ -15,7 +17,7 @@ abstract class SnowLayeredBlockMixin {
     @Definition(id = "getBrightness", method = "Lnet/minecraft/server/level/ServerLevel;getBrightness(Lnet/minecraft/world/level/LightLayer;Lnet/minecraft/core/BlockPos;)I")
     @Expression("?.getBrightness(?, ?) > ?")
     @ModifyExpressionValue(method = "randomTick", at = @At("MIXINEXTRAS:EXPRESSION"))
-    private boolean addConfigCondition(boolean original) {
+    private boolean addPermaSnowConfigRequirement(boolean original) {
         return original && PermaSnow.CONFIG.shouldSnowMelt;
     }
 
